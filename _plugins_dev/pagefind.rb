f4d3facf200e7ba342cff90f8044fb98ac7bf8af
seon.yeong <seon.yeong@webtoonscorp.com>
2025-05-01 08:48:35 +0900

    dev/prod 용 바이너리 분리

---
+++ _plugins_dev/pagefind.rb
@@ -0,0 +1,15 @@
+# https://www.bfoliver.com/2025/pagefind
+module Jekyll
+  class PostCompileCommand < Jekyll::Generator
+    safe true
+    priority :lowest
+
+    def generate(site)
+      Jekyll::Hooks.register :site, :post_write do |_site|
+        command = './_bin/pagefind_dev --site _site'
+        puts "Running: #{command}"
+        system(command)
+      end
+    end
+  end
+end
